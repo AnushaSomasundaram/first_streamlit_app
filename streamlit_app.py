9d842c1a67806d934499c62e1d763c8feb83626c
AnushaSomasundaram <58358094+AnushaSomasundaram@users.noreply.github.com>
2023-10-23 13:06:04 +0530

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,11 @@ import streamlit
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & blueberry oatmeal')
-streamlit.text('Kale, spinach and Rocket smoothie')
-streamlit.text('HArd-boiled free-range egg')
+streamlit.text('🥣 Omega 3 & blueberry oatmeal')
+streamlit.text('🥗 Kale, spinach and Rocket smoothie')
+streamlit.text('🐔HArd-boiled free-range egg')
+streamlit.text('🥑🍞Avocado toast')
+
 
 
 
