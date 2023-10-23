267e09bf5c073ec00a7d221a8fb16a023424078a
AnushaSomasundaram <58358094+AnushaSomasundaram@users.noreply.github.com>
2023-10-23 13:02:48 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('Kale, spinach and Rocket smoothie')
+streamlit.text('HArd-boiled free-range egg')
+
+
+
+
